99e64bfe0420c174309f4761c2db9c009b161f72
Glucci <Glucci@172.17.104.200>
2019-02-02 16:59:54 -0600

    merge adam keyur

--- src/main/java/org/banking/Account.java
+++ src/main/java/org/banking/Account.java
@@ -3,9 +3,13 @@ package org.banking;
 public class Account {
 
 	private String accountNumber;
-	
+	private Money balance;
 	
 	public String getAccountNumber() {
 		return accountNumber;
+	}
+	
+	public Money getBalance() {
+		return balance;
 	}
 }
---
+++ src/main/java/org/banking/Money.java
@@ -0,0 +1,5 @@
+package org.banking;
+
+public class Money {
+
+}
